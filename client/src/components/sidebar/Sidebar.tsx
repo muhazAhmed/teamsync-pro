20f0262264469058d3f6ddf8838cbba5e21af9d5
muhazAhmed <muhazvla313@gmail.com>
2024-02-20 15:04:27 +0530

    updated sideabr

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import "./sidebar.css";
-import logo from "../../assets/images/man.png";
+import userLogo from "../../assets/images/man.png";
+import logo from "../../assets/images/TS-logo1.png";
 import { Avatar } from "@nextui-org/react";
 import { useNavigate } from "react-router-dom";
+import { useSessionStorage } from "../../utils/comonFunctions";
 
 const Sidebar = () => {
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
@@ -11,6 +13,7 @@ const Sidebar = () => {
   const navigate = useNavigate();
   const openSidebar = () => setIsSidebarOpen(1);
   const closeSidebar = () => setIsSidebarOpen(0);
+  const userInfo = useSessionStorage("userInfo");
 
   const query = location.href.split("/")[3];
   useEffect(() => {
@@ -75,7 +78,7 @@ const Sidebar = () => {
     <>
       {!isHomePage && (
         <div
-          className="sidebar"
+          className="sidebar slideRight"
           style={isSidebarOpen === 1 ? { width: "0" } : {}}
         >
           {isSidebarOpen === 1 ? (
@@ -83,64 +86,79 @@ const Sidebar = () => {
           ) : (
             <i className="fa-solid fa-xmark" onClick={openSidebar}></i>
           )}
-          <img src="" alt="logo" id="logo" />
+          <img
+            src={logo}
+            alt="logo"
+            id="logo"
+            onClick={() =>
+              navigate(userInfo ? `/dashboard/${userInfo?._id}` : "/user/login")
+            }
+          />
           <div
-            className="profile"
-            style={isSidebarOpen ? { display: "none" } : {}}
+            className="profile-container"
+            style={{ position: "absolute", top: "110px", width: "100%" }}
           >
-            <Avatar src={logo} size="md" className="cursor-pointer" />
-            <h1 className="text-primary text-lg font-bold">Hi Muhaz</h1>
-            <i className="fa-solid fa-gear cursor-pointer"></i>
-          </div>
-          <div
-            className="menu-items"
-            style={isSidebarOpen ? { display: "none" } : {}}
-          >
-            {menuItems.map((item, index) => (
-              <div className="items flex-col" key={index}>
-                {item.subItems ? (
-                  <div
-                    className="drop-down"
-                    onClick={() => handleDropdownClick(index)}
-                  >
-                    <div className="items">
-                      <i className={item.icon}></i>
-                      <button
-                        className={`dropdown-btn ${
-                          activeIndex === index ? "active" : ""
-                        }`}
-                      >
-                        {item.label}
-                      </button>
+            <div
+              className="profile"
+              style={isSidebarOpen ? { display: "none" } : {}}
+            >
+              <Avatar src={userLogo} size="md" className="cursor-pointer" />
+              <h1 className="text-primary text-lg font-bold">Hi Muhaz</h1>
+              <i className="fa-solid fa-gear cursor-pointer"></i>
+            </div>
+            <div
+              className="menu-items"
+              style={isSidebarOpen ? { display: "none" } : {}}
+            >
+              {menuItems.map((item, index) => (
+                <div className="items flex-col" key={index}>
+                  {item.subItems ? (
+                    <div
+                      className="drop-down"
+                      onClick={() => handleDropdownClick(index)}
+                    >
+                      <div className="items">
+                        <i className={item.icon}></i>
+                        <button
+                          className={`dropdown-btn ${
+                            activeIndex === index ? "active" : ""
+                          }`}
+                        >
+                          {item.label}
+                        </button>
+                      </div>
+                      {activeIndex === index ? (
+                        <i className="fa fa-caret-up"></i>
+                      ) : (
+                        <i className="fa fa-caret-down"></i>
+                      )}
                     </div>
-                    {activeIndex === index ? (
-                      <i className="fa fa-caret-up"></i>
-                    ) : (
-                      <i className="fa fa-caret-down"></i>
-                    )}
-                  </div>
-                ) : (
-                  <div className="items" onClick={() => navigate(item.path)}>
-                    <i className={item.icon}></i>
-                    <p>{item.label}</p>
-                  </div>
-                )}
-                {item.subItems && (
-                  <div
-                    className="dropdown-container"
-                    style={{
-                      display: activeIndex === index ? "flex" : "none",
-                    }}
-                  >
-                    {item.subItems.map((subItem, subIndex) => (
-                      <p key={subIndex} onClick={() => navigate(subItem.path)}>
-                        {subItem.label}
-                      </p>
-                    ))}
-                  </div>
-                )}
-              </div>
-            ))}
+                  ) : (
+                    <div className="items" onClick={() => navigate(item.path)}>
+                      <i className={item.icon}></i>
+                      <p>{item.label}</p>
+                    </div>
+                  )}
+                  {item.subItems && (
+                    <div
+                      className="dropdown-container"
+                      style={{
+                        display: activeIndex === index ? "flex" : "none",
+                      }}
+                    >
+                      {item.subItems.map((subItem, subIndex) => (
+                        <p
+                          key={subIndex}
+                          onClick={() => navigate(subItem.path)}
+                        >
+                          {subItem.label}
+                        </p>
+                      ))}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       )}
