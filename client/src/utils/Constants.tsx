2d3dfe90dbd16d2b77a8f4972a1b86da0aee3290
Muhaz Ahmed <muhazvla313@gmail.com>
2024-03-26 15:17:17 +0530

    added constants

--- client/src/utils/Constants.tsx
+++ client/src/utils/Constants.tsx
@@ -3,17 +3,27 @@ export const API_URL = "https://teamsync.onrender.com/api/";    // Demo, Testing
 
 // MESSAGES:
 export const message = (name: string) => ({
+
+  // =========== Root =============
   SERVER_ERROR: "Server Under Maintenance",
+  UNAVAILABLE:"Temporarily Unavailable",
+  TRY_AGAIN: "Please Try Again After Some Time",
+
+// ============ Form ==========
   REQUIRED_FIELD: `Please Enter ${name}`,
   INVALID_EMAIL: "Please Enter Valid Email Address",
   INVALID_PASSWORD: "Password Must Have At-Least 8 Characters With Minimum 1 Number And Alphabet",
   PASSWORD_NOT_MATCHING: "Passwords Not Matching",
   LOGIN_SUCCESS: "You Have Been LoggedIn",
   LOGOUT_SUCCESS: "You Have Been LoggedOut",
+  CREATE_SUCCESS: `${name} Created Successfully`,
+  UNAUTHORIZED_USER: `You'r Not Allowed to Update ${name}`,
+
+  // =========== API =============
   GET_FAIL: "Enable To Fetch Data",
   REGISTER_SUCCESS: `${name} Registration Successful`,
-  CREATE_SUCCESS: `${name} Created Successfully`,
   REQUEST_SUBMITTED: "Your Request has been Sent",
+  USER_ERROR: `Only ${name} can perform this Action`,
 });
 
 export const Variables = {
