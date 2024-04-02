56e3c0bb1e16898f72ac4005a54e2abb709c1f51
Muhaz Ahmed <muhazvla313@gmail.com>
2024-04-02 19:16:49 +0530

    added maintenance page

---
+++ client/src/components/UnderMaintenance/Maintenance.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@nextui-org/react"
+import svg from "../../assets/svg/maintenance.svg"
+import { goBack } from "../../utils/commonFunctions"
+import "../PageNotFound/style.css"
+
+const Maintenance = () => {
+  return (
+    <div className="pageNF">
+        <img src={svg}/>
+        <h1>This Page is Under Maintenance, Please Visit After Sometime</h1>
+        <Button className="btn-ghost" onClick={goBack}>Back</Button>
+    </div>
+  )
+}
+
+export default Maintenance
