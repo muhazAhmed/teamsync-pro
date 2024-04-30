b7894de8acf187389da6e7e94b04d11259f67991
Muhaz Ahmed <muhazvla313@gmail.com>
2024-04-30 20:31:18 +0530

    added new routes

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,15 +1,17 @@
 import express from 'express';
 const routes = express.Router();
-import { updateHr, fetchOneUser, loginUser, newUser} from "../controllers/User/userController.js"
+import { fetchOneUser, loginUser, newUser, updateRequest, updateUser} from "../controllers/User/userController.js"
 import { deleteClientData, getAllClientData, getOneClientData, newClientData, updateClientData } from '../controllers/serviceController/ClientController.js';
-import { fetchAllUsers } from '../controllers/adminController/adminController.js';
+import { fetchAllUsers, fetchUserByName } from '../controllers/adminController/adminController.js';
 
 routes.post("/user/register", newUser)
 routes.post("/user/login", loginUser)
-routes.patch("/user/update/:id", updateHr)
+routes.patch("/user/update/:id/:name", updateUser)
+routes.post("/user/update-request/:id", updateRequest)
 routes.get("/user/fetch/:id", fetchOneUser)
 
 routes.get("/users/fetch-all", fetchAllUsers)
+routes.get("/users/fetch-by-name", fetchUserByName)
 
 routes.post("/client/new-request", newClientData)
 routes.get("/client/fetch-request/:id", getOneClientData)
