811c2aad70b21d93576eb5473d784572f8a88511
Muhaz Ahmed <muhazvla313@gmail.com>
2024-05-12 20:16:21 +0530

    added routes to update req

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,14 +1,19 @@
 import express from 'express';
 const routes = express.Router();
-import { fetchOneUser, loginUser, newUser, updateRequest, updateUser} from "../controllers/User/userController.js"
+import multer from "multer";
+import { fetchOneUser, loginUser, newUser, updateUser} from "../controllers/User/userController.js"
+import { fetchAllRequests, fetchPriorityCounts, updateRequest } from '../controllers/HR/HRController.js';
 import { deleteClientData, getAllClientData, getOneClientData, newClientData, updateClientData } from '../controllers/serviceController/ClientController.js';
 import { fetchAllUsers, fetchUserByName } from '../controllers/adminController/adminController.js';
 
 routes.post("/user/register", newUser)
 routes.post("/user/login", loginUser)
-routes.patch("/user/update/:id/:name", updateUser)
-routes.post("/user/update-request/:id", updateRequest)
+routes.patch("/user/update/:id", updateUser)
 routes.get("/user/fetch/:id", fetchOneUser)
+
+routes.post("/user/update-request/:id", updateRequest)
+routes.get("/user/fetch-all/update-request", fetchAllRequests)
+routes.get("/user/fetch-priority/update-request", fetchPriorityCounts)
 
 routes.get("/users/fetch-all", fetchAllUsers)
 routes.get("/users/fetch-by-name", fetchUserByName)
