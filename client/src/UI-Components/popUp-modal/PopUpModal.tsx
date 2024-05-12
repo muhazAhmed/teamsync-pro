e9f3df010f05cd12b9a10fadfe547e4d7508c9f6
Muhaz Ahmed <muhazvla313@gmail.com>
2024-05-12 20:17:38 +0530

    added button option in ui-library modal

--- client/src/UI-Components/popUp-modal/PopUpModal.tsx
+++ client/src/UI-Components/popUp-modal/PopUpModal.tsx
@@ -2,15 +2,22 @@ import "./modal.css";
 import ReactDOM from "react-dom";
 import { icon } from "../Icons/Icons";
 import { closeModal } from "../../utils/commonFunctions";
-import { Tooltip } from "@nextui-org/react";
+import { Button, Tooltip } from "@nextui-org/react";
 
 interface ModalProps {
   setModal: (value: boolean) => void;
   title: string;
   children: React.ReactNode;
+  footer?: FooterItem[];
 }
 
-const Modal: React.FC<ModalProps> = ({ setModal, title, children }) => {
+interface FooterItem {
+  label: string;
+  color?: "primary" | "danger";
+  action: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ setModal, title, children, footer }) => {
   return ReactDOM.createPortal(
     <div className="blur-bg">
       <div className="modal-container fadeIn">
@@ -25,6 +32,21 @@ const Modal: React.FC<ModalProps> = ({ setModal, title, children }) => {
         </div>
 
         <div className="modal-content">{children}</div>
+        {footer && (
+          <div className="menu-footer">
+            {footer?.map((item: FooterItem) => (
+              <Button
+                variant="shadow"
+                className={item?.color === "primary" ? "btn-primary" : ""}
+                color={item?.color}
+                onClick={item.action}
+                key={item.label}
+              >
+                {item.label}
+              </Button>
+            ))}
+          </div>
+        )}
       </div>
     </div>,
     document.body
