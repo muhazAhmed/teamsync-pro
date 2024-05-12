8fbb3539c1448f8dfe62c8f66dd5b2e44106f319
Muhaz Ahmed <muhazvla313@gmail.com>
2024-05-12 20:15:11 +0530

    added new server variable's

--- client/src/utils/serverVariables.tsx
+++ client/src/utils/serverVariables.tsx
@@ -2,8 +2,11 @@ export const serverVariables = {
     REGISTER_NEW_USER : "/user/register",
     LOGIN_USER: "/user/login",
     UPDATE_USER: "/user/update/",
-    UPDATE_REQUEST_USER: "/user/update-request/",
     FETCH_ONE_USER: "/user/fetch/",
+    
+    UPDATE_REQUEST_USER: "/user/update-request/",
+    FETCH_ALL_UPDATE_REQUESTS: "/user/fetch-all/update-request",
+    FETCH_PRIORITY_COUNTS: "/user/fetch-priority/update-request",
 
     ADMIN_FETCH_USERS: "/users/fetch-all",
     ADMIN_FETCH_USERS_BY_NAME: "/users/fetch-by-name?name=",
