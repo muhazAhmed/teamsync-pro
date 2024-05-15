3931446f59341e5d56a75aca82210af3e8cf17d1
Muhaz Ahmed <muhazvla313@gmail.com>
2024-05-15 19:14:45 +0530

    Created Delete modal  for update req

---
+++ client/src/pages/HR/SubComponents/DeleteModal.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@nextui-org/react";
+import Modal from "../../../UI-Components/popUp-modal/PopUpModal";
+import { FC } from "react";
+import { closeModal } from "../../../utils/commonFunctions";
+
+interface ModalProps {
+  setModal: (value: boolean) => void;
+  userData: any;
+}
+const DeleteModal: FC<ModalProps> = ({ setModal, userData }) => {
+  return (
+    <Modal
+      setModal={setModal}
+      title={userData?.firstName + " " + (userData?.lastName || "")}
+    >
+      <h1>Are you sure want to <span className="text-red-600">Delete</span> this request?</h1>
+      <div className="modal-footer flex" style={{ justifyContent: "flex-end" }}>
+        <Button color="danger">Delete</Button>
+        <Button className="btn-ghost" onClick={() => closeModal(setModal)}>
+          Cancel
+        </Button>
+      </div>
+    </Modal>
+  );
+};
+
+export default DeleteModal;
