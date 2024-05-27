6664405ac59a322c92acbb6ffd9db35279034239
Muhaz Ahmed <muhazvla313@gmail.com>
2024-05-27 14:23:30 +0530

    added contents

---
+++ server/src/utils/constants.js
@@ -0,0 +1,7 @@
+import { fetchDateTime } from "./helper.js";
+
+export const API_URL = `http://localhost:${process.env.PORT || 8800}/api`;
+// export const API_URL = `https://teamsync.onrender.com/api`
+
+export const fetchAllUserIds = `${API_URL}/user/fetch-all-users`;
+export const today = fetchDateTime("date").split("-").reverse().join("-");
