734adc8ecd217b7818203e4e0a6e5862599b8021
Muhaz Ahmed <muhazvla313@gmail.com>
2024-05-27 14:25:07 +0530

    added new routs

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,15 +1,17 @@
 import express from 'express';
 const routes = express.Router();
-import { fetchOneUser, loginUser, newUser, updateUser} from "../controllers/User/userController.js"
+import { fetchAllUsersId, fetchOneUser, loginUser, newUser, updateUser} from "../controllers/User/userController.js"
 import { fetchAllRequests, fetchPriorityCounts, updateRequest } from '../controllers/HR/HRController.js';
 import { deleteClientData, getAllClientData, getOneClientData, newClientData, updateClientData } from '../controllers/serviceController/ClientController.js';
 import { fetchAllUsers, fetchUserByName } from '../controllers/adminController/adminController.js';
-import { fetchOneAttendance, newAttendance, updateAttendance } from '../controllers/attendance/attendanceController.js';
+import { attendanceDashboardData, fetchAndCheckDaily, fetchOneAttendance, newAttendance, updateAttendance } from '../controllers/attendance/attendanceController.js';
+import "../controllers/serviceController/cronJobs.js"
 
 routes.post("/user/register", newUser)
 routes.post("/user/login", loginUser)
 routes.patch("/user/update/:id", updateUser)
 routes.get("/user/fetch/:id", fetchOneUser)
+routes.get("/user/fetch-all-users", fetchAllUsersId)
 
 routes.post("/user/update-request/:id", updateRequest)
 routes.get("/user/fetch-all/update-request", fetchAllRequests)
@@ -18,9 +20,12 @@ routes.get("/user/fetch-priority/update-request", fetchPriorityCounts)
 routes.post("/user/add/attendance/:id", newAttendance)
 routes.get("/user/fetch-one/attendance/:id", fetchOneAttendance)
 routes.patch("/user/update/attendance/:id", updateAttendance)
+routes.get("/user/attendance-stat/:id", attendanceDashboardData)
+routes.post("/user/attendance/daily-check/:id", fetchAndCheckDaily)
 
 routes.get("/users/fetch-all", fetchAllUsers)
 routes.get("/users/fetch-by-name", fetchUserByName)
+
 
 routes.post("/client/new-request", newClientData)
 routes.get("/client/fetch-request/:id", getOneClientData)
