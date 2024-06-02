41417755c434175f89923576f54ba647229ac7dc
Muhaz Ahmed <muhazvla313@gmail.com>
2024-06-03 01:31:52 +0530

    added new const variable

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -1,7 +1,8 @@
+import moment from "moment";
 import { fetchDateTime } from "./helper.js";
 
 // export const API_URL = `http://localhost:${process.env.PORT || 8800}/api`;
 export const API_URL = `https://teamsync.onrender.com/api`
 
 export const fetchAllUserIds = `${API_URL}/user/fetch-all-users`;
-export const today = fetchDateTime("date").split("-").reverse().join("-");
+export const today = moment().format("DD-MM-YYYY");
