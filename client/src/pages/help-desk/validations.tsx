f20028f5561f866c14eaf35cf5db1fe13d647c8e
Muhaz Ahmed <muhazvla313@gmail.com>
2024-06-09 23:42:05 +0530

    added demo access

--- client/src/pages/help-desk/validations.tsx
+++ client/src/pages/help-desk/validations.tsx
@@ -4,8 +4,12 @@ import { CheckAccess, fetchUserId } from "../../utils/commonFunctions";
 import { serverVariables } from "../../utils/serverVariables"
 
 export const handleSubmit = async (inputs: any, setLoading: any) => {
-    if (CheckAccess?.isDemoAccount) return toast.success("Request Submitted Successfully");
-    return await postMethodAPI(serverVariables?.NEW_SUPPORT_REQUEST + fetchUserId, inputs, setLoading)
+    if (CheckAccess?.isDemoAccount) {
+        return toast.success("Request Submitted Successfully");
+    } else {
+        return await postMethodAPI(serverVariables?.NEW_SUPPORT_REQUEST + fetchUserId, inputs, setLoading)
+    }
+    
 }
 
 export const categoriesArray = [
