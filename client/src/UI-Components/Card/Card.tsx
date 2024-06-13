2eedb7dabb2590a47c1d1f03051856a10c7fe4ba
Muhaz Ahmed <muhazvla313@gmail.com>
2024-06-13 20:37:35 +0530

    fixed bugs from card component

--- client/src/UI-Components/Card/Card.tsx
+++ client/src/UI-Components/Card/Card.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 interface CardProps {
   content: any;
   id?: any;
+  className?: string;
 }
 
 const customCardStyle = {
@@ -12,18 +13,18 @@ const customCardStyle = {
     minWidth: "20vw",
     boxShadow: "-1px 5px 25px var(--primary)",
     backgroundColor: "var(--card)",
-    color: "black",
+    color: "#fff",
   },
 };
 
-const Card: FC<CardProps> = ({ content, id }) => {
+const Card: FC<CardProps> = ({ content, id, className }) => {
   return (
     <div
       style={customCardStyle.container}
       className="custom-card"
       id={id ? id : undefined}
     >
-      <div>{content}</div>
+      <div className={className ? className : undefined}>{content}</div>
     </div>
   );
 };
