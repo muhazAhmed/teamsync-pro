9976aef9acafb8fcd07454e9c0f14f722ee03c4c
Muhaz Ahmed <muhazvla313@gmail.com>
2024-06-13 22:12:37 +0530

    added api  integration

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -2,10 +2,34 @@ import "./footer.css";
 import img from "../../assets/images/TS-logo1.png";
 import { icon } from "../../UI-Components/Icons/Icons";
 import { Button, Input, Tooltip } from "@nextui-org/react";
+import { useState } from "react";
+import { validEmail } from "../../utils/validation";
+import toast from "react-hot-toast";
+import { message } from "../../utils/Constants";
+import { CheckAccess } from "../../utils/commonFunctions";
+import { postMethodAPI } from "../../utils/apiCallMethods";
+import Loader from "../../UI-Components/Loader/Loader";
+import { serverVariables } from "../../utils/serverVariables";
 
 const Footer = () => {
+  const [email, setEmail] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onSubscribeClick = async () => {
+    if (validEmail(email)) return toast.error(message("").INVALID_EMAIL);
+    if (CheckAccess?.isDemoAccount) {
+      return toast.success(message("")?.SUBSCRIPTION_SUCCESS);
+    } else {
+      await postMethodAPI(
+        serverVariables?.NEW_CLIENT_REQUEST,
+        { email: email },
+        setLoading
+      );
+    }
+  };
   return (
     <div className="footer">
+      {loading && <Loader />}
       <div className="img">
         <img src={img} alt="logo" />
       </div>
@@ -40,8 +64,16 @@ const Footer = () => {
         </div>
       </div>
       <div className="subscribe">
-        <Input placeholder="abc@gmail.com" radius="full" />
-        <Button className="btn-primary">Subscribe</Button>
+        <Input
+          placeholder="abc@gmail.com"
+          radius="full"
+          name="inputs"
+          value={email}
+          onChange={(e) => setEmail(e?.target?.value)}
+        />
+        <Button className="btn-primary" onClick={onSubscribeClick}>
+          Subscribe
+        </Button>
       </div>
       <p>
         <span>®</span>2023, Muhaz Ahmed, All right reserved
