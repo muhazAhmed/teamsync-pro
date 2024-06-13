bf3900e73a9b811ef31c3e0d8fa246366ea63fbc
Muhaz Ahmed <muhazvla313@gmail.com>
2024-06-13 22:12:52 +0530

    added client req api

--- server/src/controllers/serviceController/ClientController.js
+++ server/src/controllers/serviceController/ClientController.js
@@ -1,27 +1,47 @@
 import clientModal from "../../models/services/client.js";
+import employeeModal from "../../models/Employee/employeeModel.js";
+import hrModal from "../../models/HR/hrModel.js"
+import { REQUIRE_FIELD, RESPONSE_MESSAGE, SUBSCRIPTION_ERROR } from "../../utils/validations.js";
 
 export const newClientData = async (req, res) => {
   try {
     let data = req.body;
-    let { page_id, pageName } = data;
-    if (!pageName)
-      return res.status(400).json("Server Error, Unable to create Page Name");
+    let { email, newsLetter, isRegisteredUser, employeeId } = data;
+    if (!email) return res.status(400).json(REQUIRE_FIELD("Email"));
 
-    if (pageName) {
-      const fetchLastPageId = await clientModal.find({});
-      if (fetchLastPageId.length === 0) {
-        data.page_id = pageName + "-" + 10;
-      } else {
-        const lastData = await clientModal.findOne().sort({ page_id: -1 });
-        const page = lastData.split("-")[0];
-        const newIndex = parseInt(lastData.split("-")[1]) + 1;
-        data.page_id = page + "-" + newIndex;
+    if (email) {
+      let validate = await clientModal.findOne({ email })
+      if (validate) return res.status(400).json(SUBSCRIPTION_ERROR);
+      const fetchAllClientReq = await clientModal.find();
+      let emailOnly;
+      if (fetchAllClientReq.length) {
+        emailOnly = fetchAllClientReq.map(item => item.email);
+      }
+
+      const fetchEmployees = await employeeModal.findOne({ $or: [{email}, {companyEmail: email}] })
+      if (fetchEmployees) {
+        if (emailOnly.includes(fetchEmployees.email) || emailOnly.includes(fetchEmployees.companyEmail)) 
+          return res.status(400).json(SUBSCRIPTION_ERROR);
+        data.isRegisteredUser = true;
+        data.employeeId = fetchEmployees.employeeID
+      }
+
+      const fetchHr = await hrModal.findOne({ $or: [{email}, {companyEmail: email}] })
+      if (fetchHr) {
+        if (emailOnly.includes(fetchHr.email) || emailOnly.includes(fetchHr.companyEmail)) 
+          return res.status(400).json(SUBSCRIPTION_ERROR);
+        data.isRegisteredUser = true;
+        data.employeeId = fetchHr.employeeID
       }
     }
 
+    data.newsLetter = true;
+
     const response = await clientModal.create(data);
-    return res.status(201).json(response);
+    if (response)
+      return res.status(201).json({ message: RESPONSE_MESSAGE("")?.SUBSCRIPTION_SUCCESS });
   } catch (error) {
+    console.log(error.message);
     return res.status(500).json(RESPONSE_MESSAGE("").SERVER_ERROR);
   }
 };
@@ -29,8 +49,8 @@ export const newClientData = async (req, res) => {
 export const getOneClientData = async (req, res) => {
   try {
     const id = req.params.id;
-    const fetchData = await client.findOne({ page_id, id });
-    if (!fetchData) return res.status(400).json("Page Name Not Found");
+    const fetchData = await clientModal.findOne({ email });
+    if (!fetchData) return res.status(400).json("No Data Found");
     return res.status(200).json(fetchData);
   } catch (error) {
     return res.status(500).json(RESPONSE_MESSAGE("").SERVER_ERROR);
--- server/src/models/services/client.js
+++ server/src/models/services/client.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
+import moment from "moment";
+
 const clientSchema = new mongoose.Schema(
   {
-    page_id: { type: String },
-    pageName: { type: String },
+    email: { type: String },
+    newsLetter: { type: Boolean, default: false },
+    date: { type: String, default: moment().format("DD-MM-YYYY") },
+    isRegisteredUser: { type: Boolean, default: false },
+    employeeId: {type: String, default: "NA"}
   },
   { timestamps: true }
 );
