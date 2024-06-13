16ce7333c299cef03ccf2e4ded4492893a4152dc
Muhaz Ahmed <muhazvla313@gmail.com>
2024-06-13 20:35:50 +0530

    moved navbar from root to component

---
+++ client/src/components/RootNavbar.tsx
@@ -0,0 +1,112 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  Dropdown,
+  NavbarItem,
+  DropdownTrigger,
+  Button,
+  DropdownMenu,
+  DropdownItem,
+} from "@nextui-org/react";
+import logo from "../assets/images/TS-logo1.png";
+import { icon } from "../UI-Components/Icons/Icons";
+import { logout, useSessionStorage } from "../utils/commonFunctions";
+import { useNavigate } from "react-router-dom";
+
+const RootNavbar = () => {
+  const user = useSessionStorage("userInfo");
+  const navigate = useNavigate();
+
+  const icons = {
+    Arrow: <i className="fa-solid fa-angle-down"></i>,
+    HR: <i className={icon.user}></i>,
+    AttendanceManagement: <i className={icon.calendar}></i>,
+    LeaveManagement: <i className="fa-solid fa-umbrella-beach"></i>,
+    PayRole: <i className="fa-solid fa-hand-holding-dollar"></i>,
+    SelfServe: <i className="fa-brands fa-sellcast"></i>,
+  };
+  return (
+    <Navbar>
+      <NavbarBrand>
+        <img
+          src={logo}
+          id="logo"
+          onClick={() => navigate("/home")}
+          style={{ cursor: "pointer" }}
+        />
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-5" justify="center">
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                endContent={icons.Arrow}
+                radius="sm"
+                variant="light"
+              >
+                Products
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            aria-label="ACME features"
+            className="w-[340px]"
+            itemClasses={{
+              base: "gap-4",
+            }}
+          >
+            <DropdownItem key="HR" startContent={icons.HR}>
+              HR Management
+            </DropdownItem>
+            <DropdownItem
+              key="autoscaling"
+              startContent={icons.AttendanceManagement}
+            >
+              Attendance Management
+            </DropdownItem>
+            <DropdownItem
+              key="usage_metrics"
+              startContent={icons.LeaveManagement}
+            >
+              Leave Management
+            </DropdownItem>
+            <DropdownItem key="production_ready" startContent={icons.PayRole}>
+              Pay Role Management
+            </DropdownItem>
+            <DropdownItem key="99_uptime" startContent={icons.SelfServe}>
+              Employee Self Serve
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavbarItem onClick={() => navigate("/product/about")}>
+          <h1>About</h1>
+        </NavbarItem>
+        <NavbarItem onClick={() => navigate("/product/contact")}>
+          <h1>Contact Us</h1>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button
+            color="primary"
+            className="btn-ghost"
+            variant="flat"
+            onClick={() => {
+              if (user) logout(navigate, "");
+              else navigate("/user/form");
+            }}
+          >
+            {user
+              ? [<i key="icon" className={icon.logout}></i>, " Logout"]
+              : "Login"}
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default RootNavbar;
