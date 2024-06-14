45cd70fd7ad148983714d97a7e67bf29529974e8
Muhaz Ahmed <muhazvla313@gmail.com>
2024-06-14 22:16:18 +0530

    logout modal style changes

--- client/src/components/sidebar/SubComponents/LogoutPopUpModal.tsx
+++ client/src/components/sidebar/SubComponents/LogoutPopUpModal.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@nextui-org/react";
+import { Button, Tooltip } from "@nextui-org/react";
 import { icon } from "../../../UI-Components/Icons/Icons";
 import { closeModal, logout } from "../../../utils/commonFunctions";
 import "./modal.css";
@@ -12,14 +12,25 @@ const LogoutPopUpModal: React.FC<LogoutPopUpModalProps> = ({ modalState }) => {
   const navigate = useNavigate();
   return (
     <div className="blur-bg">
-      <div className="modalContainer w-96 fadeIn" style={{ height: "30vh" }}>
-        <i className={icon.close} onClick={() => closeModal(modalState)}></i>
+      <div
+        className="logout modalContainer w-96 fadeIn"
+        style={{ height: "30vh" }}
+      >
+        <Tooltip content="Close" color="danger">
+          <i
+            className={icon.closeRounded}
+            onClick={() => closeModal(modalState)}
+            style={{ color: "#fff" }}
+          ></i>
+        </Tooltip>
         <div
           className="modalBody flex flex-col items-center gap-5 justify-center"
           style={{ height: "100%" }}
         >
           <i className={icon.logout} style={{ fontSize: "2rem" }}></i>
-          <h1 className="text-xl font-medium">Are you sure want to Logout?</h1>
+          <h1 className="text-xl font-medium" style={{ color: "#fff" }}>
+            Are you sure want to Logout?
+          </h1>
           <div className="flex items-center gap-5 justify-center">
             <Button color="danger" onClick={() => logout(navigate, modalState)}>
               Logout
