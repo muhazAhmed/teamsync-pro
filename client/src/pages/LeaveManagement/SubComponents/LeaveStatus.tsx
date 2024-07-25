1c48e851cb38bf2f47c4b76ff03a946f8beff640
Muhaz Ahmed <muhazvla313@gmail.com>
2024-07-25 17:17:07 +0530

    added leave status modal

--- client/src/pages/LeaveManagement/SubComponents/LeaveStatus.tsx
+++ client/src/pages/LeaveManagement/SubComponents/LeaveStatus.tsx
@@ -1,17 +1,67 @@
 import { FC } from "react";
-import Modal from "../../../UI-Components/popUp-modal/PopUpModal"
-import { closeModal } from "../../../utils/commonFunctions"
+import Modal from "../../../UI-Components/popUp-modal/PopUpModal";
+import { closeModal } from "../../../utils/commonFunctions";
+import "./style.css";
+import { icon } from "../../../UI-Components/Icons/Icons";
+import { Tooltip } from "@nextui-org/react";
+import { leavePendingReq } from "../../form/Demo";
 
 interface ModalProps {
   setModal: any;
+  setLoading: any;
 }
 
 const LeaveStatus: FC<ModalProps> = ({ setModal }) => {
   return (
-    <Modal title="Time-off History" setModal={() => closeModal(setModal)}>
-      <div></div>
+    <Modal
+      title="Pending History"
+      setModal={() => closeModal(setModal)}
+      className="pending-history"
+    >
+      <div className="table">
+        <table>
+          <thead>
+            <tr>
+              <th>Action</th>
+              <th>Date</th>
+              <th>Leave Type</th>
+              <th>From</th>
+              <th>To</th>
+              <th>Reason</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {leavePendingReq?.map((item: any, index: number) => (
+              <tr key={index}>
+                <td style={{ paddingLeft: "5px" }}>
+                  <Tooltip content="Withdraw Request" color="primary">
+                    <i className={icon?.closeRounded}></i>
+                  </Tooltip>
+                </td>
+                <td>{item?.date}</td>
+                <td>{item?.leaveType}</td>
+                <td>{item?.from}</td>
+                <td>{item?.to}</td>
+                <td>{item?.reason}</td>
+                <td
+                  style={
+                    item?.status === "Approved"
+                      ? { color: "#00ba00" }
+                      : item?.status === "Rejected"
+                      ? { color: "red" }
+                      : { color: "yellow" }
+                  }
+                >
+                  {item?.status}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </Modal>
-  )
-}
+  );
+};
 
-export default LeaveStatus
+export default LeaveStatus;
