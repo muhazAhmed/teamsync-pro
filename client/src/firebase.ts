7ca285e49b579734290fe014009858607e93d1c5
Muhaz Ahmed <muhazvla313@gmail.com>
2024-08-07 16:22:22 +0530

    added configs

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 import { getAnalytics } from "firebase/analytics";
+const ENV = import.meta.env
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByFt75FCvTBCYTfFw4QxTZUCE1Y34H6y8",
-  authDomain: "teamsync-pro.firebaseapp.com",
+  apiKey: ENV.VITE_FIREBASE_API_KEY,
+  authDomain: ENV.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: "teamsync-pro",
   storageBucket: "teamsync-pro.appspot.com",
-  messagingSenderId: "889752240460",
-  appId: "1:889752240460:web:44383254799ef641ef4fe9",
-  measurementId: "G-CGESD8265D"
+  messagingSenderId: ENV.VITE_FIREBASE_MESSAGE_SENDER_ID,
+  appId: ENV.VITE_FIREBASE_APP_ID,
+  measurementId: ENV.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
