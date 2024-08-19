547ea00b860fa98b8e629a2d569c592754c1a09c
muhazAhmed <muhazvla313@gmail.com>
2024-08-19 16:39:40 +0530

    added new helper function

--- server/src/utils/helper.js
+++ server/src/utils/helper.js
@@ -156,3 +156,39 @@ export const calculateTotalHours = (dataArray) => {
     });
     return convertMinutesToHourMinuteFormat(totalDuration);
 }
+
+export const generateRandomString = (length) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+export const generateRandomNumber = (length) => {
+    const numbers = '0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += numbers.charAt(Math.floor(Math.random() * numbers.length));
+    }
+    return result;
+}
+
+export const genRandomStringNumber = (length) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+export const genRandomStringNumberSpecialChar = (length) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
